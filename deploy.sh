576e4e15a026408a688a60fc4ae12d7618800ea6
yang-ym23 <yang-ym23@mails.tsinghua.edu.cn>
2025-03-11 13:42:52 +0800

    Fix: Change master to main

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@ set -e
 set -x
 
 # if remote `gitlab` does not exist, create it and point to git@git.tsinghua.edu.cn:thuse-course/course-index.git
-# if ! git remote | grep -q gitlab; then
-#   git remote add gitlab git@git.tsinghua.edu.cn:thuse-course/course-index.git
-# fi
+if ! git remote | grep -q gitlab; then
+  git remote add gitlab git@github.com:yang-ym23/KeepChat-index.git
+fi
 
 # push to remote `gitlab`
 git checkout gh-pages
 git pull origin gh-pages
 git push gitlab gh-pages
-git checkout master
+git checkout main
